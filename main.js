fe2a746ed1b33f4581f410ab9421a68af2c78f50
marijanagardovic <marijana.gardovic99@gmail.com>
2021-10-08 10:39:54 +0200

    added funcionallity for features tabs

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
-let labels = document.querySelectorAll(".items-names label");
-labels.forEach((label) => {
-	label.addEventListener("click", () => {
-		[...label.parentElement.children].forEach((siblings) =>
-			siblings.classList.remove("active-item")
-		);
-		label.classList.add("active-item");
-	});
-});
+function openTab(evt, tabName) {
+	var i, tabcontent, tablinks;
+	tabcontent = document.getElementsByClassName("tabcontent");
+	for (i = 0; i < tabcontent.length; i++) {
+	  tabcontent[i].style.display = "none";
+	}
+	tablinks = document.getElementsByClassName("tablinks");
+	for (i = 0; i < tablinks.length; i++) {
+	  tablinks[i].className = tablinks[i].className.replace(" active", "");
+	}
+	document.getElementById(tabName).style.display = "block";
+	evt.currentTarget.className += " active";
+  }
