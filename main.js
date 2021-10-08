0f91c9eede615bd1a959345d1e7bf006d4c3e28a
marijanagardovic <marijana.gardovic99@gmail.com>
2021-10-08 13:24:09 +0200

    added function for email validation

--- main.js
+++ main.js
@@ -11,4 +11,26 @@ function openTab(evt, tabName) {
 	}
 	document.getElementById(tabName).style.display = "block";
 	evt.currentTarget.className += " active";
-  }
+}
+
+  // email validation
+
+const emailRequest = document.querySelector(".request");
+
+function submitEmail(e){
+    const email = document.querySelector(".email-1");
+    const message = document.querySelector(".result");
+    const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+ 
+    if(email.value == ''){
+        message.innerText = "Make sure input field is not empty";
+    } else if(!emailRegex.test(email.value)){
+        message.innerText = "Oops! Please check your email!";
+    } else if(emailRegex.test(email.value)){
+        message.innerText = "";
+    }
+
+    e.preventDeafult();
+};
+
+emailRequest.addEventListener("click", submitEmail);
